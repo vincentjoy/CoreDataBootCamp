328bae5961c768249ad6f1c0afda12a4fa920970
Vincent Joy <vincentjoyhere@gmail.com>
2024-12-27 10:53:35 +0530

    test commit

--- CoreDataBootCamp/ContentView.swift
+++ CoreDataBootCamp/ContentView.swift
@@ -25,6 +25,7 @@ struct ContentView: View {
                     } label: {
                         Text(item.timestamp!, formatter: itemFormatter)
                     }
+                    // Test commit
                 }
                 .onDelete(perform: deleteItems)
             }
