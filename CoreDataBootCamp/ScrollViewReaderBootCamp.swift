3bee763f276c32322712769ab6f38968b818e5e0
Vincent Joy <vincentjoyhere@gmail.com>
2024-12-31 00:22:36 +0530

    added the functionality to scroll to specific index using the power of scroll view reader

--- CoreDataBootCamp/ScrollViewReaderBootCamp.swift
+++ CoreDataBootCamp/ScrollViewReaderBootCamp.swift
@@ -3,27 +3,49 @@
 import SwiftUI
 
 struct ScrollViewReaderBootCamp: View {
+    
+    @State var scrollToIndex: Int = 0
+    @State var textFieldText: String = ""
+    
     var body: some View {
-        ScrollView {
-            ScrollViewReader { proxy in
-                
-                Button("Scroll to #30") {
-                    withAnimation(.spring()) {
-                        proxy.scrollTo(29, anchor: .top)
-                    }
+        VStack(spacing: 10) {
+            
+            TextField("Enter a # here..", text: $textFieldText)
+                .padding(10)
+                .frame(height: 55)
+                .background(Color.white)
+                .cornerRadius(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.gray, lineWidth: 1)
+                )
+                .padding(.horizontal)
+                .keyboardType(.numberPad)
+            
+            Button("Scroll Now") {
+                if let index = Int(textFieldText) {
+                    scrollToIndex = index+1
                 }
-                
-                
-                ForEach(0..<50) { index in
-                    Text("This is item #\(index+1)")
-                        .font(.headline)
-                        .frame(height: 150)
-                        .frame(maxWidth: .infinity)
-                        .background(Color.white)
-                        .cornerRadius(10)
-                        .shadow(radius: 10)
-                        .padding()
-                        .id(index)
+            }
+            
+            ScrollView {
+                ScrollViewReader { proxy in
+                    ForEach(0..<50) { index in
+                        Text("This is item #\(index+1)")
+                            .font(.headline)
+                            .frame(height: 150)
+                            .frame(maxWidth: .infinity)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .shadow(radius: 10)
+                            .padding()
+                            .id(index)
+                    }
+                    .onChange(of: scrollToIndex) { oldValue, newValue in
+                        withAnimation(.spring()) {
+                            proxy.scrollTo(newValue, anchor: .top)
+                        }
+                    }
                 }
             }
         }
