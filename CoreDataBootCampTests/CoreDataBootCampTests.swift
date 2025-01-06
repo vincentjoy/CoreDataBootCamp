15416e55b7d670c3bb78a51647733d9ccbb43183
Vincent Joy <vincentjoyhere@gmail.com>
2025-01-06 19:35:11 +0530

    added the unit testing target to the project

---
+++ CoreDataBootCampTests/CoreDataBootCampTests.swift
@@ -0,0 +1,16 @@
+//
+//  CoreDataBootCampTests.swift
+//  CoreDataBootCampTests
+//
+//  Created by Vincent Joy on 06/01/25.
+//
+
+import Testing
+
+struct CoreDataBootCampTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
