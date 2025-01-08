0c6d1add2aeea140923c7ee818e2684eeaca0a92
Vincent Joy <vincentjoyhere@gmail.com>
2025-01-08 20:58:34 +0530

    ui setup complete

--- CoreDataBootCamp/UITestingBootCampView.swift
+++ CoreDataBootCamp/UITestingBootCampView.swift
@@ -3,8 +3,15 @@
 import SwiftUI
 
 @Observable final class UITestingBootCampViewModel {
+    
     @ObservationIgnored let placeHolderText: String = "Add your name.."
     var textFieldText: String = ""
+    var currentUserIsSignedIn: Bool = false
+    
+    func signupButtonPressed() {
+        guard !textFieldText.isEmpty else { return }
+        currentUserIsSignedIn = true
+    }
 }
 
 struct UITestingBootCampView: View {
@@ -19,7 +26,18 @@ struct UITestingBootCampView: View {
                 endPoint: .bottomTrailing)
             .ignoresSafeArea()
             
-            signupLayer
+            ZStack {
+                if vm.currentUserIsSignedIn {
+                    SignedInView()
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .transition(.move(edge: .trailing))
+                }
+                if !vm.currentUserIsSignedIn {
+                    signupLayer
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .transition(.move(edge: .leading))
+                }
+            }
         }
     }
 }
@@ -40,7 +58,9 @@ extension UITestingBootCampView {
                 .cornerRadius(10)
             
             Button {
-                
+                withAnimation(.spring()) {
+                    vm.signupButtonPressed()
+                }
             } label: {
                 Text("Signup")
                     .font(.headline)
@@ -52,5 +72,46 @@ extension UITestingBootCampView {
             }
         }
         .padding()
+    }
+}
+
+struct SignedInView: View {
+    
+    @State private var showAlert: Bool = false
+    
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 20) {
+                Button {
+                    showAlert.toggle()
+                } label: {
+                    Text("Show welcome alert!")
+                        .font(.headline)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .foregroundStyle(Color.white)
+                        .background(Color.red)
+                        .cornerRadius(10)
+                }
+                .alert(isPresented: $showAlert) {
+                    return Alert(title: Text("Welcome to the app!"))
+                }
+                
+                NavigationLink(
+                    destination: Text("Destination"),
+                    label: {
+                        Text("Navigate")
+                            .font(.headline)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .foregroundStyle(Color.white)
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                    }
+                )
+            }
+            .padding()
+            .navigationTitle("Welcome")
+        }
     }
 }
